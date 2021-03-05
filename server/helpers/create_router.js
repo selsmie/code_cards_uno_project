76fb2b1766ef92f6f7e8765a289964bab2a75c51
rabisse <rabisse@gmail.com>
2021-03-05 17:04:47 +0000

    base of create_router and serverjs done

---
+++ server/helpers/create_router.js
@@ -0,0 +1,20 @@
+const express = require('express');
+// const ObjectId = require('Object')
+
+const createRouter = function(collection) {
+    const router = express.Router();
+
+    router.get('/', (req, res) => {
+        collection.find().toArray()
+        .then((docs) => res.json(docs))
+        .catch((err) => {
+            res.status(500)
+            res.json({status: 500, error: err})
+        })
+    })
+
+
+    return router
+}
+
+module.exports = createRouter
---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+app.use(bodyParser.json());
+MongoClient.connect('mongodb://localhost:27017')
+    .then((client) => {
+        const db = client.db('code_cards_uno_project');
+        const cardsCollection = db.collection('cards');
+        const cardsRouter = createRouter(cardsCollection);
+        app.use('/api/code_cards', cardsRouter);
+    })
+    .catch(console.error);
+
+app.listen(3000, function() {
+    console.log(`Listening on port: ${ this.address().port }`)
+})
