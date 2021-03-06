9af3e697ff0e699e0ada5ac55220907c8b9059fe
Jenny Bulford <jennycbx@hotmail.com>
2021-03-06 12:39:53 +0000

    added components and props

--- client/src/components/AddedPlayers.vue
+++ client/src/components/AddedPlayers.vue
@@ -1,12 +1,19 @@
 <template>
-  
+  <div>
+      <player v-for="player, index in players" :key="index"
+      :player="player" />
+  </div>
 </template>
 
 <script>
-
+import Player from './Player';
 
 export default {
-
+    name: 'added-players',
+    components: {
+        'player': Player
+    },
+    props: ['playerList']
 }
 </script>
 
