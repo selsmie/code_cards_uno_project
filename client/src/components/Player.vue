a38dd721d9fbc75ed49d351ec111c798b5e6a53c
Jenny Bulford <jennycbx@hotmail.com>
2021-03-06 14:36:45 +0000

    added player.vue

---
+++ client/src/components/Player.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <p>Name: {{ player.name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'player',
+    props: ['player'],
+    methods: 
+
+}
+</script>
+
+
+
+
+<style>
+
+</style>
