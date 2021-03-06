3fae0c9f6e4b299129ab8bdfcd998efbc03dccc5
rabisse <rabisse@gmail.com>
2021-03-06 21:16:56 +0000

    got shuffle to work

--- client/src/components/PlayerForm.vue
+++ client/src/components/PlayerForm.vue
@@ -1,24 +1,29 @@
 <template>
 <div>
-    <form v-on:submit="addPlayer">
-        <label for="player_name">Player name: </label>
-        <input type="text" id="player-name" name="player-name" v-model="newPlayerName" required >
+    <form @submit="addPlayer">
+        <label for="name">Player name: </label>
+        <input type="text" id="name" name="name" v-model="name" required >
         <input type="submit" value="Add" id="save"/>
     </form>
     <br>
-    <button type="button" class="new-game-btn" v-on:click="newGame">Start new game!</button>
+    <button @click="shuffle" type="button" class="new-game-btn">Start new game!</button>
+
+    <!-- <section>
+        <added-players :players="playerList"/>
+    </section> -->
 
 </div>
 </template>
 
 <script>
 import GameService from '@/services/GameService';
+// import AddedPlayers from '@/components/AddedPlayers';
 
 export default {
     data() {
         return {
             cards: [],
-            newPlayerName: '',
+            name: '',
             players: [],
         }
     },
@@ -30,11 +35,36 @@ export default {
     methods: {
         addPlayer(evt) {
             evt.preventDefault()
-            this.players.push(this.newPlayerName)
-            eventBus.$emit('add-player', this.newPlayerName)
-            this.newPlayerName = ''
+            this.players.push({
+                name: this.name,
+                hand: []
+            })
+            this.name = ''
+        },
+
+
+        shuffle() {
+            for (let i = this.cards.length - 1; i > 0; i--) {
+                let randomIndex = Math.floor(Math.random() * i)
+                let temp = this.cards[i]
+                this.$set(this.cards, i, this.cards[randomIndex])
+                this.$set(this.cards, randomIndex, temp)
+            }
+        },
+
+        newGame(evt) {
+            evt.preventDefault()
+            // this.shuffle()
+            // console.log(this.cards);
+
+
+
+            // eventBus.$emit('new-game', this.cards, this.players)
         }
-    }
+    },
+    // components: {
+    //     'added-players': AddedPlayers
+    // }
 }
 </script>
 
