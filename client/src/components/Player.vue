6f76c1a3c224f4178d3826e5fc087f837c43af83
Jenny Bulford <jennycbx@hotmail.com>
2021-03-06 15:42:30 +0000

    Added delete button and method

--- client/src/components/Player.vue
+++ client/src/components/Player.vue
@@ -1,13 +1,24 @@
 <template>
   <div>
-      <p>Name: {{ player.name }}</p>
+      <p>Name: {{ player.name }}
+          <span>
+              <button v-on:click="handleDelete">Remove</button>
+          </span>
+      </p>
   </div>
 </template>
 
 <script>
+import { eventBus } from '../main'
+
 export default {
     name: 'player',
-    props: ['player'], 
+    props: ['player'],
+    methods: {
+        handleDelete() {
+            eventBus.$emit('delete-player', this.player)
+        }
+    } 
 }
 </script>
 
