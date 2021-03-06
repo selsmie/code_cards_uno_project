752109a8b0c7917c3e54b0ce9d499d4f7e4e8f33
Mark Burns <markburns@gmail.com>
2021-03-06 13:57:45 +0000

    Initial Commit

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,19 @@
 <template>
-<div>
+<div id=main-body>
+    <header-component></header-component>
     <h1>Hello</h1>
 </div>
 </template>
 
 <script>
 import GameService from './services/GameService'
+import HeaderComponent from './components/Header.vue'
 
 export default {
     name: 'App',
+    components: {
+        "header-component": HeaderComponent,
+    },
     data() {
         return {
             cards: []
@@ -23,5 +28,14 @@ export default {
 </script>
 
 <style>
+body {
+    margin: 0;
+}
 
+#main-body {
+    display: grid;
+    grid-template-rows: 20vh 80vh;
+    grid-template-columns: 100vw;
+    /* background-color: rgb(16, 18, 145); */
+}
 </style>
---
+++ client/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>This Is a header</h1>
+</template>
+
+<script>
+export default {
+  name: "header-component"
+
+}
+</script>
+
+<style scoped>
+* {
+  background-color: aqua;
+}
+</style>
