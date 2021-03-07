74aa7a59fea5c56c9d23fafaa73c7e7ba64407da
selsmie <simonelsmie@hotmail.com>
2021-03-07 12:12:34 +0000

    upcoming list to be tested once data input

---
+++ client/src/components/NextPlayer.vue
@@ -0,0 +1,14 @@
+<template>
+  <li>{{ player.name }} - {{ player.hand.length }}</li>
+</template>
+
+<script>
+export default {
+    name: 'next-player',
+    props: ['player']
+}
+</script>
+
+<style>
+
+</style>
---
+++ client/src/components/UpcomingPlayerList.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+      <li id='current'>{{ currentPlayer.name }} - {{ currentPlayer.hand.length }}</li>
+      <next-player :player='upNext' ></next-player>
+  </ul>
+</template>
+
+<script>
+import NextPlayer from './NextPlayer.vue'
+
+export default {
+    name: 'upcoming-player',
+    props: ['players', 'currentPlayer'],
+    components: {
+        "next-player": NextPlayer
+    },
+    computed: {
+        upNext: function() {
+            this.players.forEach((player) => {
+                if (player !== this.currentPlayer) {
+                    return player
+                }
+            })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
