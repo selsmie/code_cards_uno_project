f6492b124bbfb854cf4f47281e25b2f5a16a842d
Jenny Bulford <jennycbx@hotmail.com>
2021-03-07 13:59:18 +0000

    wrote methods and data

--- client/src/components/CardDeck.vue
+++ client/src/components/CardDeck.vue
@@ -1,18 +1,47 @@
 <template>
-  
+  <div>
+      <button class="deal-card" v-on:click="handleDeal">Deal Card</button>
+  </div>
 </template>
 
 <script>
 import { eventBus } from '../main.js'
 
+import CardsHeld from './CardsHeld.vue'
+
 export default {
     name: 'CardDeck',
     props: ['card-deck'],
     data() {
+        return {
+            remainingCardDeck: [],
+            discardPile: [],
+            currentTopCard: null,
+            selectedCard: null
+        }
         
-    },
-        
+    },  
+    methods: {
+        handleDeal() {
+            CardsHeld.push(remainingCardDeck.[1])
+        },
+
+        handleTakeTurn() {
+            discardPile.push(selectedCard)
+        },
+    mounted() {
+        eventBus.$on('card-list', (cardList) => {
+            this.remainingCardDeck = cardList
+        })
+
+        eventBus.$on('top-card', (card) => {
+            this.currentTopCard = card
+        }) 
+    }
+
+    }  
 }
+
 </script>
 
 <style>
