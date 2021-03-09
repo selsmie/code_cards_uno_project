a3595c4eed085803344308130af96ccc8e7c8a94
Mark Burns <markburns@gmail.com>
2021-03-09 10:54:24 +0000

    Leaderboard panel now displaying on menu click

--- client/src/store.js
+++ client/src/store.js
@@ -18,6 +18,22 @@ export const howToPlay = Vue.observable({
 
 export const howToPlayToggle = {
     toggleHowToPlay() {
+        console.log("HowToPlay before", howToPlay.isHowToPlayOpen)
         howToPlay.isHowToPlayOpen = !howToPlay.isHowToPlayOpen
+        console.log("HowToPlay after", howToPlay.isHowToPlayOpen)
+    }
+};
+
+
+
+export const leaderboardMenu = Vue.observable({
+    isLeaderboardMenuOpen: false
+});
+
+export const leaderboardToggle = {
+    toggleLeaderboard() {
+        console.log("leaderboard before", leaderboardMenu.isLeaderboardMenuOpen)
+        leaderboardMenu.isLeaderboardMenuOpen = !leaderboardMenu.isLeaderboardMenuOpen
+        console.log("leaderboard after", leaderboardMenu.isLeaderboardMenuOpen)
     }
 };
